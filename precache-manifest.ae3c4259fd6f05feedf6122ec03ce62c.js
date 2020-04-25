e077139721e5afed078cb9a0a0441a705d1a254e
Fumika <fumika6767@gmail.com>
2020-04-24 22:17:37 -0700

    Updates

---
+++ precache-manifest.ae3c4259fd6f05feedf6122ec03ce62c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08080d64ff46acf7f9d1d018c8e7a0ff",
+    "url": "/fumika/index.html"
+  },
+  {
+    "revision": "2b58cca7e6ab2727fac9",
+    "url": "/fumika/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5553c588edbb5986723",
+    "url": "/fumika/static/js/2.0b0ab287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fumika/static/js/2.0b0ab287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b58cca7e6ab2727fac9",
+    "url": "/fumika/static/js/main.943f5b94.chunk.js"
+  },
+  {
+    "revision": "f0f1adf6232178a9c50b",
+    "url": "/fumika/static/js/runtime-main.fadcf968.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fumika/static/media/logo.5d5d9eef.svg"
+  }
+]);
